caeaee37493df3cc48efb83c112d1851bd7ea2a9
troyflopez6 <148644191+troyflopez6@users.noreply.github.com>
2025-04-01 00:24:55 +0800

    fix(header-menu): fix header menu import

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,7 +1,7 @@
 import { APP_NAME } from '@/constants/appConfig'
-import { Menu } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
+import Menu from './components/menu'
 
 const Header = () => {
   return ( 
